8cc33ec94887e12cc72171d5e9c9251f3ab14cdf
zsenu <gstqli@inf.elte.hu>
2026-03-29 17:48:31 +0200

    urls + admin registration

--- backend/app/config/urls.py
+++ backend/app/config/urls.py
@@ -3,6 +3,12 @@ from django.urls                    import path
 from core.views                     import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from core.views                     import UserProfileView
+from core.views                     import FoodItemListView
+from core.views                     import FoodItemDetailView
+from core.views                     import StrengthExerciseListView
+from core.views                     import StrengthExerciseDetailView
+from core.views                     import CardioExerciseListView
+from core.views                     import CardioExerciseDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +17,13 @@ urlpatterns = [
     path('api/auth/login/',          TokenObtainPairView.as_view()),
 
     path('api/profiles/me/',         UserProfileView.as_view()),
+
+    path('api/food-items/',          FoodItemListView.as_view()),
+    path('api/food-items/<int:pk>/', FoodItemDetailView.as_view()),
+
+    path('api/strength-exercises/',          StrengthExerciseListView.as_view()),
+    path('api/strength-exercises/<int:pk>/', StrengthExerciseDetailView.as_view()),
+
+    path('api/cardio-exercises/',          CardioExerciseListView.as_view()),
+    path('api/cardio-exercises/<int:pk>/', CardioExerciseDetailView.as_view()),
 ]
--- backend/app/core/admin.py
+++ backend/app/core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models        import UserProfile
+from .models        import FoodItem
+from .models        import StrengthExercise
+from .models        import CardioExercise
 
 admin.site.register(UserProfile)
+admin.site.register(FoodItem)
+admin.site.register(StrengthExercise)
+admin.site.register(CardioExercise)
