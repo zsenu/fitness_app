c6d898f0e90279a8d629168773207f215c69229b
zsenu <gstqli@inf.elte.hu>
2026-04-02 21:38:00 +0200

    create_admin.py configured for .env

--- backend/app/core/management/commands/create_admin.py
+++ backend/app/core/management/commands/create_admin.py
@@ -2,6 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 import os
 
+if os.environ.get('RENDER') is None:
+    from dotenv import load_dotenv
+    from pathlib import Path
+    env_path = Path(__file__).resolve().parents[4] / '.env'
+    load_dotenv(dotenv_path = env_path)
+
 class Command(BaseCommand):
     help = 'Create default admin user'
 
@@ -11,6 +17,6 @@ class Command(BaseCommand):
         password = os.environ.get('ADMIN_PASSWORD')
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username, email, password)
-            self.stdout.write(self.style.SUCCESS(f'Admin {username} created'))
+            self.stdout.write(self.style.SUCCESS(f'Admin with username {username} created'))
         else:
-            self.stdout.write(self.style.WARNING(f'Admin {username} already exists'))
+            self.stdout.write(self.style.WARNING(f'Admin with username {username} already exists'))
