56c0f8be21fb3553867aca5f2cacd844b81a6a02
zsenu <gstqli@inf.elte.hu>
2026-05-01 01:57:20 +0200

    wildcard routing

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import type { AppDispatch } from './store/store';
 import { bootstrapAuth } from './store/thunks/authThunk';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.tsx';
 import DashboardPage from './pages/DashboardPage.tsx';
 import ProfilePage from './pages/ProfilePage.tsx';
@@ -27,6 +27,7 @@ function App() {
                 <Route path = '/dashboard' element = { <ProtectedRoute><DashboardPage /></ProtectedRoute> } />
                 <Route path = '/profile'   element = { <ProtectedRoute><ProfilePage /></ProtectedRoute> } />
                 <Route path = '/about'     element = { <AboutPage /> } />
+                <Route path = '*'          element = { <Navigate to = '/' /> } />
             </Routes>
         </BrowserRouter>
         </>
