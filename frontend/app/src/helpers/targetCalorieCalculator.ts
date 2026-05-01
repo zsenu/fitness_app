22a7fc82d8a601f90ada719aa3dbd510478efdff
zsenu <gstqli@inf.elte.hu>
2026-05-01 13:53:49 +0200

    target calorie fix

--- frontend/app/src/helpers/targetCalorieCalculator.ts
+++ frontend/app/src/helpers/targetCalorieCalculator.ts
@@ -5,6 +5,11 @@ const targetCalorieCalculator = (gender: string, birthDate: string, weight: numb
     const tdee : number = bmrTdeeCalculator(gender, birthDate, weight, height, activityLevel).tdee;
 
     const weightDifference = targetWeight - weight;
+
+    if (weightDifference === 0) {
+        return tdee;
+    }
+
     const daysUntilTarget = Math.max(1, Math.ceil((new Date(targetDate).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)));
     const dailyCalorieChange = (weightDifference * 7700) / daysUntilTarget;
 
